f8c9c94874a86ff5a472fdae48366606920f9948
Lanse <simon_stalnabb@hotmail.com>
2018-12-15 17:53:12 +0100

    Hopefully finished off the Soul Handling for Soul Shards

--- src/main/java/lanse505/mysticutils/common/compat/soulshardsrespawn/SoulHandler.java
+++ src/main/java/lanse505/mysticutils/common/compat/soulshardsrespawn/SoulHandler.java
@@ -4,13 +4,11 @@ import info.tehnut.soulshardsrespawn.api.BindingEvent;
 import info.tehnut.soulshardsrespawn.api.IBinding;
 import info.tehnut.soulshardsrespawn.api.IShardTier;
 import info.tehnut.soulshardsrespawn.core.data.Tier;
-import lanse505.mysticutils.MysticUtils;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class SoulHandler {
     @SubscribeEvent
     public void gainSoulsEvent(BindingEvent.GainSouls event) {
-        MysticUtils.getLogger().info("boop");
         IBinding binding = event.getBinding();
         int killCount = binding.getKills();
         IShardTier currentTier = binding.getTier();
